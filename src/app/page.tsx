9a329a69559424eaa83c4fc559e453e1a12248ba
Pollick0 <salman.shakhawat@outlook.com>
2024-11-12 10:53:04 -0600

    nav 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,16 @@
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [jsonData, setJsonData] = useState(String)
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch("http://localhost:3001/api/Monkey")
-      const data = await response.json()
-      console.log(data)
-      setJsonData(data)
-    }
-
-    fetchData()
-  })
 
   return (
+
+
     <div>
-      {jsonData ? JSON.stringify(jsonData): "Loading..."}
+      <nav>
+        
+      </nav>      
     </div>
+
+
   )
 }
